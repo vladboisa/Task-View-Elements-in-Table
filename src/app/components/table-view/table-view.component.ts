66deeddcf24acec27a123dbb15e005f241a3b086
Vlad Boisa <60569670+vladboisa@users.noreply.github.com>
2025-06-27 17:21:22 +0000

    feat: implement matTable with dataSource

--- src/app/components/table-view/table-view.component.ts
+++ src/app/components/table-view/table-view.component.ts
@@ -2,11 +2,12 @@ import { PeriodicElement } from './../../models/api.model';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataSourceStore } from '../../store/data-source.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-table-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './table-view.component.html',
   styleUrl: './table-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,6 +16,8 @@ export class TableViewComponent {
   private dataSource = inject(DataSourceStore);
 
   dataElements = this.dataSource.computedDataElements;
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
   ngOnInit() {
     this.dataSource.loadDataElements();
